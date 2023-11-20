042369642662c80db2951ad92001efe6ed90d829
rikardo17 <62767429+rikardo17@users.noreply.github.com>
2023-11-20 00:40:52 +0000

    Add files via upload

--- check_geolite.sh
+++ check_geolite.sh
@@ -7,5 +7,5 @@ if [ $actualsize -ge $minimumsize ]; then
 echo "$filename OK"
 echo "$actualsize"
 else
-apt-get install e2fsprogs -y && chattr -i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb ; wget http://github.com/rikardo17/geolite/raw/main/GeoLite2.mmdb -O /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb && chown xtreamcodes.xtreamcodes /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb && chattr +i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb && clear && echo "Done"
+chattr -ai /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb ; sudo chmod 0777 /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb ; rm -rf /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb ; apt-get install e2fsprogs -y && chattr -i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb ; wget http://github.com/rikardo17/geolite/raw/main/GeoLite2.mmdb -O /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb && chown xtreamcodes.xtreamcodes /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb && chattr +i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb && clear && echo "Done"
 fi
